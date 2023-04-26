c2145db6db1571adde37e8d2d90d9310e657762e
Kareem Malhis <kareem-124@hotmail.com>
2023-04-26 15:57:13 +0300

    home work done

---
+++ Assignments/hello_world.py
@@ -0,0 +1,16 @@
+# 1. TASK: print "Hello World"
+print( "Hello World" )
+# # 2. print "Hello Noelle!" with the name in a variable
+name = "Kareem"
+print( "Hello", name )	# with a comma
+print( "Hello" + name )	# with a +
+# # 3. print "Hello 42!" with the number in a variable
+name = 52
+print( "Hell", name )	# with a comma
+print( "Hello" +name )	# with a +	-- this one should give us an error! (Solve it by making number 52 as a string "52")
+# 4. print "I love to eat sushi and pizza." with the foods in variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("love to eat {} and {}".format(fave_food1,fave_food2))
+print(f"love to eat {fave_food1} and {fave_food2}") 
+
