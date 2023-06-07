6382ff4c0ca58a6777bfad220fc5a28f235ea9f4
Kareem Malhis <kareem-124@hotmail.com>
2023-06-07 23:52:26 +0300

    upload users_template

--- django/users/users_app/urls.py
+++ django/users/users_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path     
 from . import views
+app_name = 'users_app'
 urlpatterns = [
     path('', views.index),
+    path('add_user', views.add_user, name= 'add_user')
 
 ]
--- django/users/users_app/views.py
+++ django/users/users_app/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import users
 
 def index(request):
-    pass
+    context = {
+        'users_data' : users.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def add_user(request):
+    first_name_form = request.POST['first_name']
+    last_name_form = request.POST['last_name']
+    email_address_form = request.POST['email_address']
+    age_form = request.POST['age']
+
+    users.objects.create(first_name= first_name_form, last_name = last_name_form, email_address = email_address_form, age = age_form)
+    
+    return redirect('/')
