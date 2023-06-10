5e4ee092e5eceda427f688ab2b4773685f46004e
Kareem Malhis <kareem-124@hotmail.com>
2023-06-10 23:38:53 +0300

    adding Comments

--- django/books_authors_proj/books_authors_app/urls.py
+++ django/books_authors_proj/books_authors_app/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 app_name = 'books_authors_app'
 urlpatterns = [
-    path('', views.index, name='home_page'),                                # Add a New Book Page
-    path('create_author/', views.create_author, name='create_author'),       # Add a New Author Page
-    path('process', views.process, name='process'),                         # process Book/Author creation
-    path('book_details/<int:id>', views.book_details, name='book_details'), # Open Details Page for a book
-    path('add_author/', views.add_author, name='add_author'),               # Add Author to a Book
-    path('author_details/<int:id>', views.author_details, name='author_details'),
-    path('add_book/', views.add_book, name='add_book'), 
+    path('', views.index, name='home_page'),                                        # Add a New Book Page
+    path('create_author/', views.create_author, name='create_author'),              # Add a New Author Page
+    path('process', views.process, name='process'),                                 # process Book/Author creation
+    path('book_details/<int:id>', views.book_details, name='book_details'),         # Open Details Page for a book
+    path('add_author/', views.add_author, name='add_author'),                       # Add Author to a Book (Author --> Book)
+    path('author_details/<int:id>', views.author_details, name='author_details'),   # Open Details Page for an Author
+    path('add_book/', views.add_book, name='add_book'),                             # Add Book to an Author (Book --> Author)
 
 ]
--- django/books_authors_proj/books_authors_app/views.py
+++ django/books_authors_proj/books_authors_app/views.py
@@ -1,65 +1,88 @@
 from django.shortcuts import render, redirect
 from .models import *
 
+
 def index(request):
     context = {
-        'books' : book.objects.all()
+        'books': book.objects.all()
     }
     return render(request, 'index.html', context)
 
+# This method is responsible of checking whether the user is trying to add
+# a new book or a new author to the Data Base
+
+
 def process(request):
     if request.POST['process'] == 'create_book':
-        book.objects.create(title = request.POST['book_title'] , desc = request.POST['desc'])
+        book.objects.create(
+            title=request.POST['book_title'], desc=request.POST['desc'])
     elif request.POST['process'] == 'create_author':
-        author.objects.create(first_name = request.POST['author_first_name'] , last_name = request.POST['author_last_name'], notes = request.POST['notes'])
+        author.objects.create(first_name=request.POST['author_first_name'],
+                            last_name=request.POST['author_last_name'], notes=request.POST['notes'])
         return redirect('/create_author/')
     return redirect('/')
 
+# Open the Details Page of the Specified Book using its ID
+
+
 def book_details(request, id):
-    id_list =[]
-    given_book = book.objects.get(id = id)
+    id_list = []
+    given_book = book.objects.get(id=id)
     given_book_authors = given_book.authors.all()
-    rest_authors=author.objects.exclude(id__in = [o.id for o in given_book_authors])
-    #print(f"The authors of this book are : {given_book_authors}")
-    #print(f"The Rest : {rest_authors}")
+    # filter out the already existed authors and get the remaining
+    rest_authors = author.objects.exclude(
+        id__in=[o.id for o in given_book_authors])
     context = {
-        'book' : given_book,
+        'book': given_book,
         'rest_authors': rest_authors
     }
     return render(request, 'book_details.html', context)
 
+# Add Author to a Book (Author --> Book)
+
+
 def add_author(request):
-    author_id = request.POST['author_list']
-    book_id = request.POST['book_id']
-    book_selected = book.objects.get(id = book_id)
-    author_selected = author.objects.get(id = author_id)
+    author_id = request.POST['author_list']                 # Get Author ID
+    book_id = request.POST['book_id']                       # Get Book ID
+    # Select the Book record (Book object)  using Book ID
+    book_selected = book.objects.get(id=book_id)
+    # Select the Author record (Author object) using Author ID
+    author_selected = author.objects.get(id=author_id)
+    # Add the selected Author record to the Selected Book
     book_selected.authors.add(author_selected)
+    # redirect to the same page to refresh the page
     return redirect(f'/book_details/{book_id}')
+
 
 def create_author(request):
     context = {
-        'authors' : author.objects.all()
+        'authors': author.objects.all()
     }
     return render(request, 'author_create.html', context)
 
+
 def add_book(request):
-    author_id = request.POST['author_id']
-    book_id = request.POST['book_list']
-    book_selected = book.objects.get(id = book_id)
-    author_selected = author.objects.get(id = author_id)
+    author_id = request.POST['author_id']                   # Get Author ID
+    book_id = request.POST['book_list']                     # Get Book ID
+    # Select the Book record (Book object)  using Book ID
+    book_selected = book.objects.get(id=book_id)
+    # Select the Author record (Author object) using Author ID
+    author_selected = author.objects.get(id=author_id)
+    # Add the selected Book record to the Selected Author
     book_selected.authors.add(author_selected)
+    # redirect to the same page to refresh the page
     return redirect(f'/author_details/{author_id}')
 
+
 def author_details(request, id):
-    id_list =[]
-    given_author = author.objects.get(id = id)
+    id_list = []
+    given_author = author.objects.get(id=id)
     given_author_books = given_author.books.all()
-    rest_books=book.objects.exclude(id__in = [o.id for o in given_author_books])
-    #print(f"The authors of this book are : {given_book_authors}")
-    #print(f"The Rest : {rest_authors}")
+    # filter out the already existed Books and get the remaining
+    rest_books = book.objects.exclude(
+        id__in=[o.id for o in given_author_books])
     context = {
-        'author' : given_author,
+        'author': given_author,
         'rest_books': rest_books
     }
     return render(request, 'author_details.html', context)
-
