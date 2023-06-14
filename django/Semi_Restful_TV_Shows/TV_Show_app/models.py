251382d15da00ab5e90a3bcbca5464220045e3b0
Kareem Malhis <kareem-124@hotmail.com>
2023-06-14 19:21:51 +0300

    Upload with Validation

--- django/Semi_Restful_TV_Shows/TV_Show_app/models.py
+++ django/Semi_Restful_TV_Shows/TV_Show_app/models.py
@@ -1,7 +1,39 @@
 from django.db import models
+import datetime
+
+# Validation Section:
+class Validate(models.Manager):
+    def new_show_validate(self, Data):
+        errors={}
+        # Validate : Title has at least 2 characters
+        if len(Data['title']) < 2:
+            errors['name'] = "Title Should be at least 2 Characters"
+        # Validate : The Title is Unique 
+        check_title = Show.objects.filter(title = str(Data['title']))
+        if check_title.count() > 0:
+            errors['title_exists'] = "This Title Already exists"
+        # Validate : NetWork has at least 3 characters
+        if len(Data['network'])  < 3:
+            errors['network'] = "Network should be at least 3 characters"
+        # Validate : If user entered a description it has at least 10 characters
+        if len(Data['desc']) < 10 and len(Data['desc']) != 0:
+            errors['desc'] = "Descriptions Should be at least 10 characters"
+        # Try: Get The Date and compare it with the current date
+        try:
+            # Get Current Date
+            date = datetime.datetime.now()
+            # Strip The (POST date) we got from the user to prepare it for dates comparison operation
+            new_release_date = datetime.datetime.strptime(Data['release_date'], '%Y-%m-%d')
+            # If the Current date is smaller than Release Date: add an error massage
+            if date < new_release_date:
+                errors['release_date'] = "The release date must be in the past"
+        # If error ocurred during compiling Then the user didn't enter a release date!
+        except:
+            errors['release_date'] = "Please enter a release date"
+
+        return errors
 
 
-# Create your models here.
 class Show(models.Model):
     title = models.CharField(max_length=100)
     network = models.CharField(max_length=255)
@@ -9,3 +41,6 @@ class Show(models.Model):
     release_date = models.DateField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = Validate()
+
+
--- django/Semi_Restful_TV_Shows/TV_Show_app/views.py
+++ django/Semi_Restful_TV_Shows/TV_Show_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from TV_Show_app.models import Show
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -12,13 +13,20 @@ def new_show(request):
     return render(request, 'new_show.html')
 
 def create_new_show(request):
-    #Validation Section will be here
-    Show.objects.create(title =request.POST['new_title']
-                        ,network = request.POST['new_network']
-                        ,desc = request.POST['new_desc']
-                        ,release_date = request.POST['new_release_date']
-                        ) 
-    return redirect('/')
+    # Validate Creating new Show
+    errors = Show.objects.new_show_validate(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request,value)
+        return redirect('/show/new')
+    else:
+        Show.objects.create(title =request.POST['title']
+                            ,network = request.POST['network']
+                            ,desc = request.POST['desc']
+                            ,release_date = request.POST['release_date']
+                            ) 
+        id = Show.objects.last()
+        return redirect('/show/' + str(id.id))
 
 def show_details(request, id):
     context = {
@@ -27,19 +35,30 @@ def show_details(request, id):
     return render(request, 'show_details.html', context)
 
 def show_edit(request, id):
+    show = Show.objects.get(id = id)
+    date = str(show.release_date)
+    #value="2013-01-08" This is the format we need to send to the input type date in html in order to be able to change its value
     context = {
-        'show' : Show.objects.get(id = id)
+        'show' : show,
+        'date' : date,
     }
     return render (request, 'show_edit.html',context)
 
 def show_update(request,id):
-    show_to_update = Show.objects.get(id = id)
-    show_to_update.title = request.POST['edit_title']
-    show_to_update.network = request.POST['edit_network']
-    show_to_update.release_date = request.POST['edit_release_date']
-    show_to_update.desc = request.POST['edit_desc']
-    show_to_update.save()
-    return redirect('/')
+    # Validate Editing Show
+    errors = Show.objects.new_show_validate(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request,value)
+        return redirect('/show/'+ str(id) +'/edit')
+    else:
+        show_to_update = Show.objects.get(id = id)
+        show_to_update.title = request.POST['title']
+        show_to_update.network = request.POST['network']
+        show_to_update.release_date = request.POST['release_date']
+        show_to_update.desc = request.POST['desc']
+        show_to_update.save()
+        return redirect('/')
 
 def show_delete(request,id):
     show_to_delete = Show.objects.get(id = id)
